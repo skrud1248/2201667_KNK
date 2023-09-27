eba620e4c00a0c60af2f39968d04c2f3475d44fe
skrud1248 <146170736+skrud1248@users.noreply.github.com>
2023-09-27 16:33:37 +0900

    Add files via upload

---
+++ 2201667 09_27.js
@@ -0,0 +1,21 @@
+console.log('my'+' cat');
+console.log('1' + 2);
+console.log('string literals:1 + 2 = ${1 +2}');
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 / 1);
+console.log(1 * 1);
+console.log(5 % 2);
+console.log(2 ** 3);
+
+console.log(10 < 6);
+console.log(10 <= 6);
+console.log(10 > 6);
+console.log(10 >= 6);
+
+const value1 = false;
+const value2 = 4 < 2;
+
+console.log(value1 || value2);
+console.log(value1 && value2);
